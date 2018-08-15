f2fb7dde8368672cdb33d596124441e3eb2d6662
Neha Chopade <nehachopade22@gmail.com>
2018-08-15 00:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution here!
+const drivers=["Milo","Otis","Garfield"];
+function destructivelyAppendDriver(firstName){
+  /*const newDrivers=drivers.slice()*/
+drivers.push(firstName)
+}
+
+function destructivelyPrependDriver(secondName)
+{
+  drivers.pop(secondName)
+}
